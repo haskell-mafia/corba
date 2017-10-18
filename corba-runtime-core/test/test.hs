fc920e9fd8b7bf97631c0f5cc3abe2950ce3f61b
Tim Humphries <tim.humphries@ambiata.com>
2017-10-18 19:55:08 +1100

    Remove disorder

--- corba-runtime-core/test/test.hs
+++ corba-runtime-core/test/test.hs
@@ -1,9 +1,15 @@
-
-import           Disorder.Core.Main
+import           System.Exit
 import qualified Test.Corba.Runtime.Core.Json as Json
 
 main :: IO ()
 main =
-  disorderMain [
+  runner [
       Json.tests
     ]
+
+runner :: [IO Bool] -> IO ()
+runner tests = do
+  b <- fmap and (sequence tests)
+  if b
+    then exitSuccess
+    else exitFailure
